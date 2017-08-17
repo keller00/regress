6fd0f40e3d774175c1d1f7fe5fb37e2f4c3e1f0c
Mark Keller <markooo.keller@gmail.com>
2017-08-17 19:55:53 -0400

    Added verbose levels, reworked debug messages

--- regress/regress.py
+++ regress/regress.py
@@ -6,6 +6,14 @@ import os
 import glob
 import subprocess
 import sys
+OPTIONS = {
+    'COMMAND': None,
+    'IN': '',
+    'OUT': '',
+    'VERBOSE': 0,
+    'PATH': '.',
+    'ERROR': False
+}
 
 class colors:
     HEADER = '\033[95m'
@@ -17,18 +25,22 @@ class colors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 def print_warnings(msg):
     print colors.WARNING + msg
+
 
 def print_errors(msg):
     print colors.FAIL + msg
 
+
 def print_errors_or_warnings(msg):
-    if ERROR:
+    if OPTIONS['ERROR']:
         print_errors(msg)
         return True
     else:
         print_warnings(msg)
+
 
 def which(pgm):
     path = os.getenv('PATH')
@@ -37,70 +49,71 @@ def which(pgm):
         if os.path.exists(p) and os.access(p, os.X_OK):
             return p
 
-# Verbose output
-def debug(string):
-    if VERBOSE:
+
+# Verbose output levels:
+#   0 - normal use, should only output errors
+#   1 - verbose for normal users, should output successful tests too
+#   2 - verbose for developers, should output all the info
+def debug(level, string):
+    if level <= OPTIONS['VERBOSE']:
         print string
+
 
 def main():
     parser = argparse.ArgumentParser(description='Run a program with multiple input files')
     parser.add_argument('-i', '--in', help='Input file prefix', default='in')
     parser.add_argument('-o', '--out', help='Output file prefix', default='out')
     parser.add_argument('-p', '--path', help='Path to input/output files', default='.')
-    parser.add_argument('-v', '--verbose', help='Increase output verbosity', action='store_true')
+    parser.add_argument('-v', '--verbose', help='Increase output verbosity', action='count')
     parser.add_argument('-e', '--error', help='Change warnings to errors', action='store_true')
     parser.add_argument('command', help='Command to run with input files')
     args = vars(parser.parse_args())
 
     # Set Constants
-    COMMAND = args['command']
-    IN = args['in']
-    OUT = args['out']
-    VERBOSE = args['verbose']
-    PATH = args['path']
-    ERROR = args['error']
+    OPTIONS['COMMAND'] = args['command']
+    OPTIONS['IN'] = args['in']
+    OPTIONS['OUT'] = args['out']
+    OPTIONS['VERBOSE'] = args['verbose']
+    OPTIONS['PATH'] = args['path']
+    OPTIONS['ERROR'] = args['error']
 
-    debug(args)
+    debug(2, "Arguments: " + str(args))
 
     # Check if command is valid
-    command = COMMAND.split(" ")[0]
-    debug("Checking if " + command + " is available...")
+    command = OPTIONS['COMMAND'].split(" ")[0]
+    debug(1, "Checking if " + command + " is available...")
     path_file = which(command)  # Command is somewhere in path
     local_file = os.path.isfile(command)  # Command is a local file
     if not (path_file or local_file):
         print_errors("Command \"" + command + "\" does not exist")
-        debug("Terminating Program.")
         sys.exit(100)
 
     # Find all input/output files
-    input_files = glob.glob(os.path.join(PATH, IN + '*'))
-    output_files = glob.glob(os.path.join(PATH, OUT + '*'))
+    input_files = glob.glob(os.path.join(OPTIONS['PATH'], OPTIONS['IN'] + '*'))
 
-    debug("Checking path: " + os.path.join(PATH, IN + '*'))
-    debug("Detected input Files:")
-    debug(input_files)
+    debug(2, "Checking path: " + os.path.join(OPTIONS['PATH'], OPTIONS['IN'] + '*'))
+    debug(1, "Detected input Files:" + str(input_files))
 
     valid_pairs = []
     terminate_program = False
 
-    # Make
     for test in input_files:
-        debug("Checking " + test + "'s output file")
-        suffix = test.rsplit(IN)[-1]
-        outpath = os.path.join(PATH, OUT + suffix)
+        debug(2, "Checking " + test + "'s output file")
+        suffix = test.rsplit(OPTIONS['IN'])[-1]
+        outpath = os.path.join(OPTIONS['PATH'], OPTIONS['OUT'] + suffix)
         if not os.path.isfile(outpath):
             terminate_program = terminate_program or print_errors_or_warnings("Output file does not exist: " + outpath)
         else:
             valid_pairs.append((test, outpath))
-            debug("Output file found: " + outpath)
+            debug(2, "Output file found: " + outpath)
 
     if terminate_program:
-        debug("Terminating Program.")
+        debug(2, "Terminating Program.")
         sys.exit(101)
 
     for test, outpath in valid_pairs:
         input_file = open(test)
-        process = subprocess.Popen(COMMAND, stdin=input_file, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process = subprocess.Popen(OPTIONS['COMMAND'], stdin=input_file, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
 
         # Read in expected output
@@ -111,6 +124,7 @@ def main():
                 # Output differs from expected
                 print_errors_or_warnings("Output of " + test + " differs from the expected output of " + outpath)
                 break
+        debug(1, 'Output of ' + OPTIONS['COMMAND'] + ' ' + test + ' matches ' + outpath)
 
-if (__name__ == '__main__'):
+if __name__ == '__main__':
     main()
